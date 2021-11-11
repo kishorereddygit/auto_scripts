d9351e6c7de74f8798e560c6034f1f532a64de9c
Srinivasan <91538297+Srinivasan-78@users.noreply.github.com>
2021-11-11 17:49:12 +0530

    Update Installdeps.sh

--- Installdeps.sh
+++ Installdeps.sh
@@ -19,13 +19,12 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo apt-get install libffi-dev libssl-dev -y
 sudo apt install python3-dev -y
 sudo apt-get install -y python3 python3-pip -y
-pip uninstall docker-compose
+sudo pip3 uninstall docker-compose
 sudo pip3 install docker-compose -y
-sudo apt update
 
 #Manage Docker as a non-root user
 sudo usermod -aG docker $USER
 newgrp docker 
 docker run hello-world
 docker --version
-echo \Reboot your Pi
+echo \Update and Reboot your Pi
