2d794586b0064d93a5e7b80e659dc8486a665b96
Srinivasan <91538297+Srinivasan-78@users.noreply.github.com>
2021-11-15 16:32:20 +0530

    Update Install_Dependencies.sh

--- Install_Dependencies.sh
+++ Install_Dependencies.sh
@@ -16,9 +16,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-sudo apt-get install libffi-dev libssl-dev -y
-sudo apt install python3-dev -y
-sudo apt-get install -y python3 python3-pip -y
 sudo pip3 install docker-compose
 #Manage Docker as a non-root user
 sudo usermod -aG docker $USER
