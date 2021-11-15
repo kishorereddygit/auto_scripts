3279451a1660faa790f24e79e69ce12b3bc0ef2b
Srinivasan <91538297+Srinivasan-78@users.noreply.github.com>
2021-11-15 15:36:20 +0530

    Update Unpack_in_pi.sh

--- On_to_pi/Unpack_in_pi.sh
+++ On_to_pi/Unpack_in_pi.sh
@@ -21,36 +21,6 @@ CERT_TOOL_DIR="$HOME/chip-certification-tool"
 APPS="$HOME/apps"
 
 mkdir $APPS
-#install Dependencies
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt-get install git gcc g++ python pkg-config libssl-dev libdbus-1-dev \
-     libglib2.0-dev libavahi-client-dev ninja-build python3-venv python3-dev \
-     python3-pip unzip libgirepository1.0-dev libcairo2-dev python3-pip zip pi-bluetooth -y
-
-#Install docker and docker compose
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
- echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-sudo apt-get install libffi-dev libssl-dev -y
-sudo apt install python3-dev -y
-sudo apt-get install -y python3 python3-pip -y
-sudo pip3 install docker-compose
-sudo apt update
-
-#Manage Docker as a non-root user
-sudo usermod -aG docker $USER
-newgrp docker 
-docker run hello-world
-docker --version
 
 # Move chip binaries to home
 mv "$ROOT_DIR/chip-tool" "$APPS/" 
