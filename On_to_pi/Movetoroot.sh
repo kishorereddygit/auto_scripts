e9ac8956b5bc8e4a5304e7657a72d8b4665e1627
Srinivasan <91538297+Srinivasan-78@users.noreply.github.com>
2021-11-23 15:53:56 +0530

    Create Movetoroot.sh

---
+++ On_to_pi/Movetoroot.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+#
+# Copyright (c) 2021 Project CHIP Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+mv DDCommand.sh ~
+mv Unpack_in_pi.sh	~
+mv chip-bridge-app ~		
+mv chip-lighting-app ~      
+mv chip-ota-requestor-app ~  
+mv chip-tool-ipv6only ~  
+mv chip-tv-casting-app ~	
+mv python_lib ~  
+mv start-test-harness.sh ~
+mv Install_Dependencies.sh ~  
+mv chip-all-clusters-app ~	
+mv chip-certification-tool.tar.gz ~	
+mv chip-ota-provider-app ~  
+mv chip-tool ~	       
+mv chip-tv-app ~	   
+mv python_env ~		
+mv rc.local ~   
+mv thermostat-app ~
