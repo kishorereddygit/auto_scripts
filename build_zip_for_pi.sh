a6de9c3ec7af4a03e008ed886b23faea949a697a
Srinivasan <91538297+Srinivasan-78@users.noreply.github.com>
2021-11-24 12:21:30 +0530

    new

--- build_zip_for_pi.sh
+++ build_zip_for_pi.sh
@@ -105,12 +105,10 @@ cd "$CERT_TOOL_ROOT"
 git-archive-all -v "$OUT_DEST_PATH/chip-certification-tool.tar.gz"
 
 # Copy helper scripts
-cp "$ROOT_DIR/On_to_pi/Movetoroot.sh" $OUT_DEST_PATH/
-cp "$ROOT_DIR/DDCommand.sh" $OUT_DEST_PATH/
+
+
 cp "$ROOT_DIR/Install_Dependencies.sh" $OUT_DEST_PATH/
-cp "$ROOT_DIR/On_to_pi/Unpack_in_pi.sh" $OUT_DEST_PATH/
-cp "$ROOT_DIR/On_to_pi/rc.local" $OUT_DEST_PATH/
-cp "$ROOT_DIR/On_to_pi/start-test-harness.sh" $OUT_DEST_PATH/
+cp "$ROOT_DIR/on_to_pi" $OUT_DEST_PATH/
 
 # compress as zip
 cd "$ROOT_DIR"
