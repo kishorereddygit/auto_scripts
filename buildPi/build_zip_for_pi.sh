912b45adf3ee7484405ccb3f8fdf5b3eda5d67f7
Srinivasan <91538297+Srinivasan-78@users.noreply.github.com>
2021-12-13 15:59:42 +0530

    Update build_zip_for_pi.sh

--- buildPi/build_zip_for_pi.sh
+++ buildPi/build_zip_for_pi.sh
@@ -98,6 +98,8 @@ mv "$PYTHON_ENV_OUT" $OUT_DEST_PATH/
 PYTHON_LIB_OUT="$CHIP_ROOT/out/python_lib"
 mv "$PYTHON_LIB_OUT" $OUT_DEST_PATH/
 
+sudo pip3 install git-archive-all
+
 # Archive Test Harness repo
 cd "$CERT_TOOL_ROOT"
 git-archive-all -v "$OUT_DEST_PATH/chip-certification-tool.tar.gz"
