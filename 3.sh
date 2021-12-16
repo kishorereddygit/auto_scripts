1eefb7a01764426ee5625ffa85324737753e6076
Srinivasan <91538297+Srinivasan-78@users.noreply.github.com>
2021-12-16 14:57:44 +0530

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -20,6 +20,8 @@ mkdir -p "$OUT_DEST_PATH"
 ./$CHIP_ROOT/gn_build.sh
 mv $GN_PATH $OUT_DEST_PATH
 
+CHIP_TOOL_ROOT="$CHIP_ROOT/examples/chip-tool"
+CHIP_TOOL_EXEC_PATH="$CHIP_TOOL_ROOT/out/host"
 "$CHIP_ROOT/scripts/examples/gn_build_example.sh" "$CHIP_TOOL_ROOT" "$CHIP_TOOL_IPV6ONLY_EXEC_PATH" 'chip_mdns="platform" chip_inet_config_enable_ipv4=false'
 mv $CHIP_TOOL_IPV6ONLY_EXEC_PATH/chip-tool $OUT_DEST_PATH/chip-tool-ipv6only
 
