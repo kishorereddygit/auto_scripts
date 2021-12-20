77993993dfc9d08b509a8738faf943c0581ad069
Srinivasan <91538297+Srinivasan-78@users.noreply.github.com>
2021-12-20 23:48:27 +0530

    Update for building apps

--- buildPi/build_zip_for_pi.sh
+++ buildPi/build_zip_for_pi.sh
@@ -34,39 +34,10 @@ then
 fi
 mkdir -p "$OUT_DEST_PATH"
 
-# chip-tool
-CHIP_TOOL_ROOT="$CHIP_ROOT/examples/chip-tool"
-CHIP_TOOL_EXEC_PATH="$CHIP_TOOL_ROOT/out/host"
-CHIP_TOOL_IPV6ONLY_EXEC_PATH="$CHIP_TOOL_ROOT/out-ipv6-only/host"
-"$CHIP_ROOT/scripts/examples/gn_build_example.sh" "$CHIP_TOOL_ROOT" "$CHIP_TOOL_EXEC_PATH" 'chip_mdns="platform"'
-mv $CHIP_TOOL_EXEC_PATH/chip-tool $OUT_DEST_PATH/chip-tool
-
-"$CHIP_ROOT/scripts/examples/gn_build_example.sh" "$CHIP_TOOL_ROOT" "$CHIP_TOOL_IPV6ONLY_EXEC_PATH" 'chip_mdns="platform" chip_inet_config_enable_ipv4=false'
-mv $CHIP_TOOL_IPV6ONLY_EXEC_PATH/chip-tool $OUT_DEST_PATH/chip-tool-ipv6only
-
-# Sample app: all-cluster
-ALL_CLUSTER_ROOT="$CHIP_ROOT/examples/all-clusters-app/linux"
-ALL_CLUSTER_EXEC_PATH="$ALL_CLUSTER_ROOT/out/host"
-"$CHIP_ROOT/scripts/examples/gn_build_example.sh" "$ALL_CLUSTER_ROOT" "$ALL_CLUSTER_EXEC_PATH"
-mv $ALL_CLUSTER_EXEC_PATH/chip-all-clusters-app $OUT_DEST_PATH/
-
-# Sample app: Lighting
-LIGHTING_ROOT="$CHIP_ROOT/examples/lighting-app/linux"
-LIGHTING_EXEC_PATH="$LIGHTING_ROOT/out/host"
-"$CHIP_ROOT/scripts/examples/gn_build_example.sh" "$LIGHTING_ROOT" "$LIGHTING_EXEC_PATH" 
-mv $LIGHTING_EXEC_PATH/chip-lighting-app $OUT_DEST_PATH/
-
-# Sample app: Bridge
-BRIDGE_ROOT="$CHIP_ROOT/examples/bridge-app/linux"
-BRIDGE_EXEC_PATH="$BRIDGE_ROOT/out/host"
-"$CHIP_ROOT/scripts/examples/gn_build_example.sh" "$BRIDGE_ROOT" "$BRIDGE_EXEC_PATH" 
-mv $BRIDGE_EXEC_PATH/chip-bridge-app $OUT_DEST_PATH/
-
-# Sample app: Thermosat
-THERMOSTAT_ROOT="$CHIP_ROOT/examples/thermostat/linux"
-THERMOSTAT_EXEC_PATH="$THERMOSTAT_ROOT/out/host"
-"$CHIP_ROOT/scripts/examples/gn_build_example.sh" "$THERMOSTAT_ROOT" "$THERMOSTAT_EXEC_PATH" 
-mv $THERMOSTAT_EXEC_PATH/thermostat-app $OUT_DEST_PATH/
+cd $CHIP_ROOT
+./gn_build.sh
+mv out apps
+mv apps $OUT_DEST_PATH/
 
 # Sample app: OTA Provider
 OTA_PROVIDER_ROOT="$CHIP_ROOT/examples/ota-provider-app/linux"
@@ -79,18 +50,6 @@ OTA_REQUESTOR_ROOT="$CHIP_ROOT/examples/ota-requestor-app/linux"
 OTA_REQUESTOR_EXEC_PATH="$OTA_REQUESTOR_ROOT/out/host"
 "$CHIP_ROOT/scripts/examples/gn_build_example.sh" "$OTA_REQUESTOR_ROOT" "$OTA_REQUESTOR_EXEC_PATH" 'chip_config_network_layer_ble=false'
 mv $OTA_REQUESTOR_EXEC_PATH/chip-ota-requestor-app $OUT_DEST_PATH/
-
-# Sample app: TV
-TV_ROOT="$CHIP_ROOT/examples/tv-app/linux"
-TV_EXEC_PATH="$TV_ROOT/out/host"
-"$CHIP_ROOT/scripts/examples/gn_build_example.sh" "$TV_ROOT" "$TV_EXEC_PATH"
-mv $TV_EXEC_PATH/chip-tv-app $OUT_DEST_PATH
-
-# Sample app: TV-casting
-TV_CASTING_ROOT="$CHIP_ROOT/examples/tv-casting-app/linux"
-TV_CASTING_EXEC_PATH="$TV_CASTING_ROOT/out/host"
-"$CHIP_ROOT/scripts/examples/gn_build_example.sh" "$TV_CASTING_ROOT" "$TV_CASTING_EXEC_PATH"
-mv $TV_CASTING_EXEC_PATH/chip-tv-casting-app $OUT_DEST_PATH
 
 "$CHIP_ROOT/scripts/build_python.sh" --chip_detail_logging true --chip_mdns platform
 PYTHON_ENV_OUT="$CHIP_ROOT/out/python_env"
