0b41b53d8d4ab8ac36112d34d439f2e8433fcb35
Srinivasan <91538297+Srinivasan-78@users.noreply.github.com>
2021-12-24 15:38:04 +0530

    Update 5.sh

--- 5.sh
+++ 5.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Scripts for starting docker automatically on boot
-sudo mv "$ROOT_DIR/artifact/rc.local.service" /etc/systemd/system/rc-local.service
+#sudo mv "$ROOT_DIR/artifact/rc.local.service" /etc/systemd/system/rc-local.service
 sudo chmod +x /etc/rc.local
 sudo systemctl enable rc-local
-sudo systemctl start rc-local.service
+#sudo systemctl start rc-local.service
