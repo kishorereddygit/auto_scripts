7dec271a99e572b999a75b1e1ac538027819452e
Srinivasan <91538297+Srinivasan-78@users.noreply.github.com>
2021-12-28 15:27:19 +0530

    Update setting_up.sh

--- setting_up.sh
+++ setting_up.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-sudo usermod -aG docker $USER
-newgrp docker
 git clone --recursive git@github.com:CHIP-Specifications/chip-certification-tool.git
 cd chip-certification-tool/frontend/
 git checkout develop
@@ -13,3 +11,5 @@ echo "Edit these files: sudo nano /etc/systemd/system/dbus-fi.w1.wpa_supplicant1
 echo " and sudo nano /etc/wpa_supplicant/wpa_supplicant.conf and REBOOT "
 cd ~/chip-certification-tool/
 git log
+sudo usermod -aG docker $USER
+newgrp docker
