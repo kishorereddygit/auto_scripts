1406a06a059f658d519fcf48087c86c96b3015b2
Srinivasan <91538297+Srinivasan-78@users.noreply.github.com>
2021-12-28 15:22:17 +0530

    New Set of scripts to try

--- install_dependency.sh
+++ install_dependency.sh
@@ -1,27 +1,11 @@
 #!/usr/bin/env bash
-#
-# Copyright (c) 2021 Project CHIP Authors
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-cd ../
-mv buildPi ~
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt-get install git gcc g++ python pkg-config libssl-dev libdbus-1-dev \
      libglib2.0-dev libavahi-client-dev ninja-build python3-venv python3-dev \
      python3-pip unzip libgirepository1.0-dev libcairo2-dev python3-pip zip pi-bluetooth -y
-
+sudo apt install default-jre -y
+sudo apt install default-jdk -y
 #Install docker and docker compose
 sudo apt-get install \
     ca-certificates \
@@ -34,6 +18,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-sudo usermod -aG docker $USER
-newgrp docker
 sudo pip3 install docker-compose
+echo "REBOOT PI"
+echo "ssh-keygen -o"
