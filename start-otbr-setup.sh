16ba9a46fd916179b9abddc44d2361ace768749f
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-02-25 00:27:58 +0530

    Update start-otbr-setup.sh

--- start-otbr-setup.sh
+++ start-otbr-setup.sh
@@ -1,7 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-cd otbr
-./otbr_run.sh
-echo "Application is starting..."
-sleep 20
-echo "OTBR is up and running..."
+RADIO=/dev/ttyACM0
+while getopts :r: flag
+do
+    case "${flag}" in
+        r) RADIO=${OPTARG};;
+    esac
+done
+
+echo "Using radio device: $RADIO (override by providing a different device with flag -r. Eg. \"-r /dev/ttyACM0\""
+echo "Will start web interface on port 8080"
+sleep 2
+sudo docker run -it --rm --network host --privileged -v $RADIO:/dev/radio connectedhomeip/otbr:te8 --radio-url spinel+hdlc+uart:///dev/radio -B wlan0
