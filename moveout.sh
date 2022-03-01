49fcec877978472ff99c55f208edb4e6737f0b57
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-03-01 11:37:44 +0530

    moving out files to root

---
+++ moveout.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+mv 1install_dependency.sh ~
+mv 2setting_up.sh ~
+mv 3docker_all.sh ~
+mv 4building_apps.sh ~
+mv rc.local ~
+mv start-otbr-setup.sh ~
+mv start-test-harness.sh ~
+cd ..
+rm -rf auto_scripts/
