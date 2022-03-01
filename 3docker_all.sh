c6ca0bed54a9ef0b54b2daed36b2cdaac6ece75e
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-03-01 14:56:35 +0530

    Update 3docker_all.sh

--- 3docker_all.sh
+++ 3docker_all.sh
@@ -6,3 +6,5 @@ cd backend/
 ./scripts/build_managed_docker_images.sh
 cd ..
 docker-compose up
+echo "Edit these files: sudo nano /etc/systemd/system/dbus-fi.w1.wpa_supplicant1.service "
+echo " and sudo nano /etc/wpa_supplicant/wpa_supplicant.conf and REBOOT "
