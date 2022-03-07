a550b9d6ef33598562333a51a1e70f27e7e91cb2
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-03-07 10:45:57 +0530

    Create remove_dependencies.sh

---
+++ remove_dependencies.sh
@@ -0,0 +1,11 @@
+sudo apt-get uninstall git gcc g++ python pkg-config libssl-dev libdbus-1-dev \
+     libglib2.0-dev libavahi-client-dev ninja-build python3-venv python3-dev \
+     python3-pip unzip libgirepository1.0-dev libcairo2-dev python3-pip zip pi-bluetooth -y
+sudo apt uninstall default-jre -y
+sudo apt uninstall default-jdk -y
+#Install docker and docker compose
+sudo apt-get uninstall \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y
