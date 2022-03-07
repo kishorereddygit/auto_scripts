6826215c5e051a674ed4edac8ce18092647525e4
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-03-07 11:07:00 +0530

    added other dependencies to be removed

--- remove_dependencies.sh
+++ remove_dependencies.sh
@@ -1,16 +1,16 @@
-sudo apt-get uninstall git gcc g++ python pkg-config libssl-dev libdbus-1-dev \
+sudo apt-get remove git gcc g++ python pkg-config libssl-dev libdbus-1-dev \
      libglib2.0-dev libavahi-client-dev ninja-build python3-venv python3-dev \
      python3-pip unzip libgirepository1.0-dev libcairo2-dev python3-pip zip pi-bluetooth -y
-sudo apt uninstall default-jre -y
-sudo apt uninstall default-jdk -y
+sudo apt remove default-jre -y
+sudo apt remove default-jdk -y
 #Install docker and docker compose
-sudo apt-get uninstall \
+sudo apt-get remove \
     ca-certificates \
     curl \
     gnupg \
     lsb-release -y
-sudo apt-get uninstall docker-ce docker-ce-cli containerd.io -y
-sudo pip3 uninstall docker-compose
-sudo apt-get uninstall pi-bluetooth avahi-utils -y
-sudo apt uninstall net-tools -y
-sudo apt-get uninstall linux-modules-extra-raspi -y
+sudo apt-get remove docker-ce docker-ce-cli containerd.io -y
+sudo pip3 remove docker-compose
+sudo apt-get remove pi-bluetooth avahi-utils -y
+sudo apt remove net-tools -y
+sudo apt-get remove linux-modules-extra-raspi -y
