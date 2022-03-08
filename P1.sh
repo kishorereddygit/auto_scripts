14440bcdb9f5fb05d0ce9e64907c036b26cb02c3
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-03-08 17:03:16 +0530

    removing unwanted files

---
+++ P1.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+mv Building_Apps.sh ~
+mv Install_Deps.sh ~
+mv Setting_Up.sh ~
+cd ~
+rm -rf PI2
+rm start-otbr-setup.sh
+rm remove_dependencies.sh
+rm start-test-harness.sh 
