1d5116113cf7c39b848b53fda0723f6d6d17b4df
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-03-08 16:46:29 +0530

    Adding required files
    This include the frontend, backend.

---
+++ PI2/Setting_all_Up.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+git clone --recursive git@github.com:CHIP-Specifications/chip-certification-tool.git
+cd chip-certification-tool/frontend/
+git checkout develop
+cd ..
+cd backend/
+git checkout develop
+rm -rf third_party
