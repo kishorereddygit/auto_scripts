c605dffad0f4eca0a24ed076e915f756cb7e9116
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-03-09 11:25:33 +0530

    added connectedhomeip symlink

--- PI2/config.sh
+++ PI2/config.sh
@@ -3,6 +3,7 @@
 cd ~
 sudo cp chip-certification-tool/backend/test_environment.config.example  chip-certification-tool/backend/test_environment.config
 ln -s chip-certification-tool/backend/test_environment.config test-harness.config
+ln -s chip-certification-tool/backend/third_party/connectedhomeip/repo connectedhomeip
 ln -s /etc/netplan/50-cloud-init.yaml network.config
 ln -s chip-certification-tool/cli cli
 sudo mv rc.local /etc/
