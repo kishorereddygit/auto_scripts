cc66e9a59593718f95367e394e4231ff5a9cb4b7
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-03-09 14:39:39 +0530

    changed docker cmd

--- start-otbr-setup.sh
+++ start-otbr-setup.sh
@@ -11,4 +11,4 @@ done
 echo "Using radio device: $RADIO (override by providing a different device with flag -r. Eg. \"-r /dev/ttyACM0\""
 echo "Will start web interface on port 8080"
 sleep 2
-sudo docker run -it --rm --network host --privileged -v $RADIO:/dev/radio connectedhomeip/otbr:te8 --radio-url spinel+hdlc+uart:///dev/radio -B wlan0
+sudo docker run --sysctl "net.ipv6.conf.all.disable_ipv6=0 net.ipv4.conf.all.forwarding=1 net.ipv6.conf.all.forwarding=1" -p 8080:80 --dns=127.0.0.1 -it --volume /dev/ttyACM0:/dev/ttyACM0 --privileged connectedhomeip/otbr:te8 --radio-url spinel+hdlc+uart:///dev/ttyACM0
