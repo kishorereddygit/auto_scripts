0f1ccd6f6d38271c02d974f692e4e78b7ba8857b
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-03-21 18:04:12 +0530

    Update Install_Deps.sh

--- PI1/Install_Deps.sh
+++ PI1/Install_Deps.sh
@@ -4,9 +4,10 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt-get install git gcc g++ python pkg-config libssl-dev libdbus-1-dev \
      libglib2.0-dev libavahi-client-dev ninja-build python3-venv python3-dev \
-     python3-pip unzip libgirepository1.0-dev libcairo2-dev python3-pip zip pi-bluetooth -y
+     python3-pip unzip libgirepository1.0-dev libcairo2-dev zip pi-bluetooth -y
 sudo apt install default-jre -y
 sudo apt install default-jdk -y
+pip3 install pigweed -y
 sudo apt-get install pi-bluetooth avahi-utils -y
 sudo apt-get install linux-modules-extra-raspi -y
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
