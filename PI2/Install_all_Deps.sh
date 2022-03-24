7d2b140bef9a5ed97f7b695d5c3314bf47597327
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-03-24 15:05:31 +0530

    Update Install_all_Deps.sh

--- PI2/Install_all_Deps.sh
+++ PI2/Install_all_Deps.sh
@@ -7,6 +7,7 @@ sudo apt-get install git gcc g++ python pkg-config libssl-dev libdbus-1-dev \
      python3-pip unzip libgirepository1.0-dev libcairo2-dev python3-pip zip pi-bluetooth -y
 sudo apt install default-jre -y
 sudo apt install default-jdk -y
+
 #Install docker and docker compose
 sudo apt-get install \
     ca-certificates \
@@ -18,6 +19,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
+
+#docker installation
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo pip3 install docker-compose
 sudo apt-get install pi-bluetooth avahi-utils -y
@@ -25,6 +28,16 @@ sudo apt install net-tools -y
 sudo apt-get install linux-modules-extra-raspi -y
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
 sudo apt install generate-ninja -y
+
+#installing deps for dd
+sudo apt-get update -y
+sudo apt-get install -y dcfldd
+
+#installing pishrink
+wget https://raw.githubusercontent.com/Drewsif/PiShrink/master/pishrink.sh
+chmod +x pishrink.sh
+sudo mv pishrink.sh /usr/local/bin
+
 sudo modprobe ip6table_filter
 sudo usermod -aG docker $USER
 newgrp docker
