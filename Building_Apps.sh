07b3309bb148b2a34fa8515c38f22e8d91840b2d
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-03-29 14:28:00 +0530

    Building apps and adding symbolic links

--- Building_Apps.sh
+++ Building_Apps.sh
@@ -16,15 +16,15 @@ scripts/examples/gn_build_example.sh examples/ota-requestor-app/linux examples/o
 scripts/examples/gn_build_example.sh examples/door-lock-app/linux/ out/door-lock-app chip_inet_config_enable_ipv4=false
 cd ~
 cd apps
-mv ../connectedhomeip/out/debug/chip-tool chip-tool
-mv ../connectedhomeip/out/debug/chip-shell chip-shell
-mv ../connectedhomeip/out/debug/chip-cert chip-cert
-mv ../connectedhomeip/examples/all-clusters-app/linux/out/all-clusters-app/chip-all-clusters-app all-cluster-app
-mv ../connectedhomeip/examples/lighting-app/linux/out/lighting-app/chip-lighting-app lighting-app
-mv ../connectedhomeip/examples/tv-casting-app/linux/out/tv-casting-app/chip-tv-casting-app tv-casting-app
-mv ../connectedhomeip/examples/tv-app/linux/out/tv-app/chip-tv-app tv-app
-mv ../connectedhomeip/examples/bridge-app/linux/out/bridge-app/chip-bridge-app bridge-app
-mv ../connectedhomeip/examples/thermostat/linux/out/thermostat/thermostat-app thermostat-app
-mv ../connectedhomeip/examples/ota-requestor-app/linux/out/host/chip-ota-requestor-app ota-requestor-app
-mv ../connectedhomeip/examples/ota-provider-app/linux/out/host/chip-ota-provider-app ota-provider-app
-mv ../connectedhomeip/out/door-lock-app/chip-door-lock-app door-lock-app
+ln -s ../connectedhomeip/out/debug/chip-tool chip-tool
+ln -s ../connectedhomeip/out/debug/chip-shell chip-shell
+ln -s ../connectedhomeip/out/debug/chip-cert chip-cert
+ln -s ../connectedhomeip/examples/all-clusters-app/linux/out/all-clusters-app/chip-all-clusters-app all-cluster-app
+ln -s ../connectedhomeip/examples/lighting-app/linux/out/lighting-app/chip-lighting-app lighting-app
+ln -s ../connectedhomeip/examples/tv-casting-app/linux/out/tv-casting-app/chip-tv-casting-app tv-casting-app
+ln -s ../connectedhomeip/examples/tv-app/linux/out/tv-app/chip-tv-app tv-app
+ln -s ../connectedhomeip/examples/bridge-app/linux/out/bridge-app/chip-bridge-app bridge-app
+ln -s ../connectedhomeip/examples/thermostat/linux/out/thermostat/thermostat-app thermostat-app
+ln -s ../connectedhomeip/examples/ota-requestor-app/linux/out/host/chip-ota-requestor-app ota-requestor-app
+ln -s ../connectedhomeip/examples/ota-provider-app/linux/out/host/chip-ota-provider-app ota-provider-app
+ln -s ../connectedhomeip/out/door-lock-app/chip-door-lock-app door-lock-app
