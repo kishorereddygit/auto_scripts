53438116e0ba4d247f33a636f6ce30a29399d69e
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-03-29 15:23:45 +0530

    continuous building workflow

--- auto_start.sh
+++ auto_start.sh
@@ -1,1 +1,14 @@
-
+FILE=/home/ubuntu/flag.txt
+if test -f "$FILE"; then
+bash ./docker_Setup.sh
+bash ./building_Apps.sh
+rm flag.txt
+else
+bash ./dependencies.sh
+bash ./move_to_root.sh
+bash ./test_harness.sh
+bash ./connectedhomeip.sh
+bash ./config.sh
+touch flag.txt
+sudo reboot
+fi
