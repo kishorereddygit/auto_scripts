b50d6ecd32cf46ea2221b7d49008246058a8b5e7
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-03-29 20:36:39 +0530

    Update move_to_root.sh

--- move_to_root.sh
+++ move_to_root.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 mv artifacts/ ~
-mv README.md building_Apps.sh config.sh connectedhomeip.sh dd.sh dependencies.sh docker_Setup.sh move_to_root.sh test_harness.sh ~ 
+mv README.md building_Apps.sh config.sh connectedhomeip.sh dd.sh dependencies.sh docker_Setup.sh move_to_root.sh test_harness.sh auto_start.sh ~ 
 rm -rf auto_scripts
