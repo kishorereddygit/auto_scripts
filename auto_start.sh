b882df6dccfa6103995b258369f208f915790ec7
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-03-30 14:12:21 +0530

    added kernal patch for depedency installation

--- auto_start.sh
+++ auto_start.sh
@@ -5,7 +5,7 @@ bash ./building_Apps.sh
 rm flag.txt
 dd.sh v_latest
 else
-bash ./kernal_patch.sh
+sudo bash ./kernal_patch.sh
 rm kernal_patch.sh  
 bash ./dependencies.sh &&
 bash ./move_to_root.sh &&
