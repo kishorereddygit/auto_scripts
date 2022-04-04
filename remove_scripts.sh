da11dd5479d42556f57177c92610dfcbca277e89
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-04-04 18:05:29 +0530

    Create remove_scripts.sh

---
+++ remove_scripts.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd ~
+rm README.md 
+rm auto_start.sh 
+rm building_Apps.sh 
+rm config.sh 
+rm dependencies.sh 
+rm docker_Setup.sh 
+rm move_to_root.sh 
+rm test_harness.sh 
+rm connectedhomeip.sh 
