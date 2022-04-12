3dd5342dfd483301f9ad8c558643d68754e53eb8
kishorereddygit <kkreddy027@gmail.com>
2022-04-12 13:50:26 +0530

    Update building_Apps.sh

--- building_Apps.sh
+++ building_Apps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd ~
+cd /home/ubuntu
 cd connectedhomeip/
 source ./scripts/bootstrap.sh
 gn gen out/debug --args='chip_mdns="platform" chip_inet_config_enable_ipv4=false'
@@ -14,7 +14,7 @@ scripts/examples/gn_build_example.sh      examples/thermostat/linux/      exampl
 scripts/examples/gn_build_example.sh examples/ota-provider-app/linux examples/ota-provider-app/linux/out/host 'chip_config_network_layer_ble=false'
 scripts/examples/gn_build_example.sh examples/ota-requestor-app/linux examples/ota-requestor-app/linux/out/host 'chip_config_network_layer_ble=false'
 scripts/examples/gn_build_example.sh examples/door-lock-app/linux/ out/door-lock-app chip_inet_config_enable_ipv4=false
-cd ~
+cd /home/ubuntu
 cd apps
 ln -s ../connectedhomeip/out/debug/chip-tool chip-tool
 ln -s ../connectedhomeip/out/debug/chip-shell chip-shell
